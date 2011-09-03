13017f58dbcbbbc93fcfe30dfa50db4d8997e49e
Anand Kumria <akumria@acm.org>
2011-09-03 14:18:38 +0100

    remove project name reference

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,8 +6,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^myproject/', include('myproject.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

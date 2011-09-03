b2a07ede8d5e4fa12a6b109b4495f5b2ce1ddc13
Anand Kumria <akumria@acm.org>
2011-09-03 14:12:55 +0100

    Activate the admin and south

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,9 +135,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    # 3rd-party
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging

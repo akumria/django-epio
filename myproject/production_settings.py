6459f7f1d7c4d2e9e6f322096d30db0b3dddf893
Anand Kumria <akumria@acm.org>
2011-10-26 18:07:41 +0100

    Switch to specifying the Database settings manually.
     We have to do the same for redis, as a cache, so we might as well
     do it for everything.

--- myproject/production_settings.py
+++ myproject/production_settings.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 from settings import *
+from bundle_config import config
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -9,3 +10,14 @@ EMAIL_BACKEND = 'django_ses.SESBackend'
 SITE_ID = 1
 
 MEDIA_ROOT = os.path.join(os.getenv('EPIO_DATA_DIRECTORY'), 'media')
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": config['postgres']['host'],
+        "PORT": int(config['postgres']['port']),
+        "USER": config['postgres']['username'],
+        "PASSWORD": config['postgres']['password'],
+        "NAME": config['postgres']['database'],
+    },
+}
